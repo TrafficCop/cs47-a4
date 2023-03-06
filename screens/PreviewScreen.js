34ed076f6d65450a3aabe8872003b59b8fcbafe5
TrafficCop <103712563+TrafficCop@users.noreply.github.com>
2023-03-05 17:34:01 -0800

    changes

--- screens/PreviewScreen.js
+++ screens/PreviewScreen.js
@@ -3,7 +3,6 @@ import React from 'react'
 import {WebView} from 'react-native-webview'
 
 const PreviewScreen = ({route}) => {
-  //<Text>{route.params.url}</Text>
   return (
     <WebView startInLoadingState={true}  style = {{ width: Dimensions.get('window').width, height: Dimensions.get('window').height}}  source={{ uri: route.params.url}}  />
   )
